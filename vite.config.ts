8ca662818f6966d217c04cb20ffda22397a059c4
Juan Correa <juan.correa.herrera@gmail.com>
2024-09-03 11:14:27 -0600

    chore: add vite-plugin-dts for generating TypeScript declaration files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
