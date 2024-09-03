b687f837c5697ec386d83bd0e6e41190b1fd0ffe
Juan Correa <juan.correa.herrera@gmail.com>
2024-09-03 11:12:07 -0600

    chore: add vite-plugin-lib-inject-css and include the css in the dist bundled

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./main.css";
+
 export const helloLibrary = () => {
   console.log("Hello, Library!");
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
